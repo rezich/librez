a8482c680bcb66920b2fef051386aecca16ae94e
Adam Rezich <adam@rezich.com>
2023-12-11 14:20:19 -0700

    added time functions

--- src/Util.h
+++ src/Util.h
@@ -79,30 +79,94 @@ Hash hash_pointer(const void* ptr) {
 #endif
 }
 
+
+//TODO: clean up the date/time stuff below
+
+
 #define Timestamp(s, ms) (Timestamp){ (s), (ms) }
 typedef struct {
     unsigned int s;
     unsigned int ms;
 } Timestamp;
+
 #define Timespan(s, ms) (Timespan){ (s), (ms) }
 typedef struct {
     unsigned int s;
     unsigned int ms;
 } Timespan;
+
+typedef struct {
+    unsigned int year;        // 2000+
+    unsigned int month;       // 1–12
+    unsigned int day;         // 1–31
+    unsigned int day_of_week; // 0—6
+    unsigned int hour;        // 0—23
+    unsigned int minute;      // 0–59
+    unsigned int second;      // 0–59
+    unsigned int millisecond; // 0–999
+} Moment;
+
+const char* WEEKDAY_SHORT[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+FORCE_INLINE bool year_is_leap_year(unsigned int year) { return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0); }
+
+const unsigned int SECONDS_PER_MINUTE      = 60;
+const unsigned int MINUTES_PER_HOUR        = 60;
+const unsigned int SECONDS_PER_HOUR        = 3600;
+const unsigned int HOURS_PER_DAY           = 24;
+const unsigned int DAYS_PER_WEEK           = 7;
+const unsigned int DAYS_PER_YEAR           = 365;
+const unsigned int DAYS_PER_LEAP_YEAR      = 366;
+const unsigned int SECONDS_PER_DAY         = 86400;
+const unsigned int EPOCH_YEAR              = 2000;
+const unsigned int EPOCH_DAY_OF_WEEK       = 6;
+const unsigned int MILLISECONDS_PER_SECOND = 1000;
+
+void timestamp_parse(Timestamp timestamp, Moment* moment) {
+    unsigned int total_days = timestamp.s / SECONDS_PER_DAY;
+    *moment = (Moment){ .year = EPOCH_YEAR, .month = 1 };
+    
+    while (total_days >= (year_is_leap_year(moment->year) ? DAYS_PER_LEAP_YEAR : DAYS_PER_YEAR)) {
+           total_days -= (year_is_leap_year(moment->year) ? DAYS_PER_LEAP_YEAR : DAYS_PER_YEAR);
+        ++moment->year;
+    }
+    const unsigned int   DAYS_IN_MONTH[] = { 31, 28 + (year_is_leap_year(moment->year) ? 1 : 0), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    while (total_days >= DAYS_IN_MONTH[moment->month - 1]) {
+           total_days -= DAYS_IN_MONTH[moment->month - 1];
+        ++moment->month;
+    }
+
+    moment->day = total_days + 1;
+    moment->day_of_week = (EPOCH_DAY_OF_WEEK + timestamp.s / SECONDS_PER_DAY) % DAYS_PER_WEEK;
+
+    unsigned int      remaining_seconds = timestamp.s % SECONDS_PER_DAY;
+    moment->hour   =  remaining_seconds / (SECONDS_PER_MINUTE * MINUTES_PER_HOUR);
+    moment->minute = (remaining_seconds % (SECONDS_PER_MINUTE * MINUTES_PER_HOUR)) / SECONDS_PER_MINUTE;
+    moment->second =  remaining_seconds %  SECONDS_PER_MINUTE;
+    moment->millisecond = timestamp.ms;
+
+    assert(moment->millisecond <= 999);
+    assert(moment->second      <= 59);
+    assert(moment->minute      <= 59);
+    assert(moment->hour        <= 23);
+    assert(moment->day         <= 31);
+    assert(moment->day_of_week <= 6);
+    assert(moment->month       <= 12);
+}
+
 void timestamp_now(Timestamp* timestamp) {
     timestamp->s = pd->system->getSecondsSinceEpoch(&timestamp->ms);
 }
+
 unsigned int timestamp_diff(Timestamp* earlier, Timestamp* later, Timespan* diff) {
-    unsigned int ms_diff = (later->s - earlier->s) * 1000 + later->ms - earlier->ms;
+    unsigned int ms_diff = (later->s - earlier->s) * MILLISECONDS_PER_SECOND + later->ms - earlier->ms;
     if (diff) {
-        diff->s  = ms_diff / 1000;
-        diff->ms = ms_diff % 1000;
+        diff->s  = ms_diff / MILLISECONDS_PER_SECOND;
+        diff->ms = ms_diff % MILLISECONDS_PER_SECOND;
     }
     return ms_diff;
 }
-#define SECONDS_PER_DAY    86400
-#define SECONDS_PER_HOUR   3600
-#define SECONDS_PER_MINUTE 60
+
 void timespan_parse(Timespan timespan, unsigned int* days, unsigned int* hours, unsigned int* minutes, unsigned int* seconds, unsigned int* milliseconds) {
     if (days)         *days         = timespan.s / SECONDS_PER_DAY;
     timespan.s                     %= SECONDS_PER_DAY;
@@ -112,6 +176,12 @@ void timespan_parse(Timespan timespan, unsigned int* days, unsigned int* hours, 
     timespan.s                     %= SECONDS_PER_MINUTE;
     if (seconds)      *seconds      = timespan.s;
     if (milliseconds) *milliseconds = timespan.ms;
+}
+
+void timestamp_add_timespan(Timestamp* timestamp, Timespan timespan) {
+    timestamp->ms += timespan.ms;
+    timestamp->s  += timespan.s + timestamp->ms / MILLISECONDS_PER_SECOND;
+    timestamp->ms %= MILLISECONDS_PER_SECOND;
 }
 
 
